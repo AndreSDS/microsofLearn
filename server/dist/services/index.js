0594bfef71aa62aaa92a89ab5a78b34321441ad6
andresds <andre_smiths@outlook.com>
2020-04-21 02:24:42 -0300

    subindo arquivos

---
+++ server/dist/services/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vacation_service_1 = __importDefault(require("./vacation.service"));
+exports.vacationService = vacation_service_1.default;
+//# sourceMappingURL=index.js.map
